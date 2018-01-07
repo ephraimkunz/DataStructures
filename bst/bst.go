a9bee3553f5fc52b209af489231cd70639537b87
Ephraim Kunz <ephraimkunz@me.com>
2018-01-07 11:22:45 -0700

    bst done

--- bst/bst.go
+++ bst/bst.go
@@ -1,28 +1,188 @@
 // Package bst implements a binary search tree.
 package bst
 
-type Node struct {
-	left  *Node
-	right *Node
+import (
+	"strconv"
+	"strings"
+)
+
+type BST struct {
+	root  *node
+	count int
+}
+
+type node struct {
 	value int
+	left  *node
+	right *node
 }
 
-func NewBST(rootVal int) Node {
-	n := Node{nil, nil, rootVal}
-	return n
+// NewBST creates and returns a new, empty binary search tree
+func NewBST() *BST {
+	return &BST{}
 }
 
-func (n *Node) Exists(item int) bool {
-
+// Exists returns true if item in bst, else false.
+func (bst *BST) Exists(item int) bool {
+	node, _ := recursiveFind(item, bst.root)
+	return node != nil
 }
 
-func (n *Node) Remove(item int) (Node, bool) {
+// Remove removes item from bst and returns true if succeeded,
+// false if item not in bst.
+func (bst *BST) Remove(item int) bool {
+	child, parent := recursiveFind(item, bst.root)
 
+	if child == nil { // Not in tree
+		return false
+	}
+
+	if parent == nil { // Removing root
+		if bst.root.left != nil {
+			right := bst.root.right
+			bst.root = bst.root.left
+
+			// Put right side in correct location
+			insertPos := bst.root
+			for ; insertPos.right != nil; insertPos = insertPos.right {
+			}
+			insertPos.right = right
+		} else if bst.root.right != nil {
+			left := bst.root.left
+			bst.root = bst.root.right
+
+			// Put left side in correct location
+			insertPos := bst.root
+			for ; insertPos.left != nil; insertPos = insertPos.left {
+			}
+			insertPos.left = left
+		} else {
+			bst.root = nil
+		}
+		bst.count--
+		return true
+	}
+	//return false
+
+	if parent.left == child {
+		if child.left != nil {
+			right := child.right
+			parent.left = child.left
+
+			// Put right side in correct location
+			insertPos := parent.left
+			for ; insertPos.right != nil; insertPos = insertPos.right {
+			}
+			insertPos.right = right
+		} else if child.right != nil {
+			left := child.left
+			parent.left = child.right
+
+			// Put left side in correct location
+			insertPos := parent.left
+			for ; insertPos.left != nil; insertPos = insertPos.left {
+			}
+			insertPos.left = left
+		} else {
+			parent.left = nil
+		}
+		bst.count--
+		return true
+	} else {
+		if child.left != nil {
+			right := child.right
+			parent.right = child.left
+
+			// Put right side in correct location
+			insertPos := parent.right
+			for ; insertPos.right != nil; insertPos = insertPos.right {
+			}
+			insertPos.right = right
+		} else if child.right != nil {
+			left := child.left
+			parent.right = child.right
+
+			// Put left side in correct location
+			insertPos := parent.right
+			for ; insertPos.left != nil; insertPos = insertPos.left {
+			}
+			insertPos.left = left
+		} else {
+			parent.right = nil
+		}
+		bst.count--
+		return true
+	}
 }
 
-func (n *Node) Add(item int) (Node, bool) {
+// Searchest tree with root node for item. If item is found,
+// child points to it and parent points to its parent. If the found
+// node is the root of the entire tree, the parent is nil. If the item is not found,
+// child is nil and parent holds the parent of where the child should go.
+func recursiveFind(item int, root *node) (child, parent *node) {
+	if root == nil {
+		return nil, nil
+	}
+
+	if item == root.value { // Found item is the root of the entire tree
+		return root, nil
+	}
+
+	if item < root.value {
+		if root.left == nil {
+			return nil, root
+		} else if item == root.left.value {
+			return root.left, root
+		} else {
+			return recursiveFind(item, root.left)
+		}
+	} else {
+		if root.right == nil {
+			return nil, root
+		} else if item == root.right.value {
+			return root.right, root
+		} else {
+			return recursiveFind(item, root.right)
+		}
+	}
 }
 
-func (n *Node) IsBST() bool {
+// Insert inserts the item correctly into the bst, but does not tree balancing.
+// No duplicates are allowed, so Insert returns false if item is a duplicate.
+func (bst *BST) Insert(item int) bool {
+	// First item
+	if bst.root == nil {
+		bst.root = &node{item, nil, nil}
+		bst.count++
+		return true
+	}
 
+	child, parent := recursiveFind(item, bst.root)
+	if child != nil {
+		return false
+	}
+
+	if item < parent.value {
+		parent.left = &node{item, nil, nil}
+	} else {
+		parent.right = &node{item, nil, nil}
+	}
+	bst.count++
+	return true
+}
+
+// For checking state in testing
+func (bst *BST) statePrint() string {
+	result := statePrintRecursive(bst.root)
+
+	// Strip off possible leading and trailing whitespace
+	return strings.TrimSpace(result)
+}
+
+func statePrintRecursive(root *node) string {
+	if root == nil { // Base case
+		return ""
+	}
+
+	return statePrintRecursive(root.left) + " " + strconv.Itoa(root.value) + statePrintRecursive(root.right)
 }
--- bst/bst_test.go
+++ bst/bst_test.go
@@ -1,15 +1,55 @@
 package bst
 
 import (
-	"reflect"
 	"testing"
 )
 
-func TestNode_Exists(t *testing.T) {
+func TestBST_Insert(t *testing.T) {
 	type fields struct {
-		left  *Node
-		right *Node
-		value int
+		root  *node
+		count int
+	}
+	type args struct {
+		item int
+	}
+	tests := []struct {
+		name      string
+		fields    fields
+		args      args
+		want      bool
+		wantCount int
+		wantState string
+	}{
+		{"Fails on duplicate", fields{&node{5, nil, nil}, 1}, args{5}, false, 1, "5"},
+		{"Inserts on empy", fields{nil, 0}, args{5}, true, 1, "5"},
+		{"Inserts on left", fields{&node{5, &node{3, nil, nil}, nil}, 2}, args{1}, true, 3, "1 3 5"},
+		{"Inserts on right", fields{&node{5, &node{3, nil, nil}, &node{7, nil, nil}}, 3}, args{8}, true, 4, "3 5 7 8"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			bst := &BST{
+				root:  tt.fields.root,
+				count: tt.fields.count,
+			}
+			if got := bst.Insert(tt.args.item); got != tt.want {
+				t.Errorf("BST.Insert() = %v, want %v", got, tt.want)
+			}
+
+			if bst.count != tt.wantCount {
+				t.Errorf("BST.Insert() = %v, want count %v", bst.count, tt.wantCount)
+			}
+
+			if bst.statePrint() != tt.wantState {
+				t.Errorf("BST.Insert() = %v, want state %v", bst.statePrint(), tt.wantState)
+			}
+		})
+	}
+}
+
+func TestBST_Exists(t *testing.T) {
+	type fields struct {
+		root  *node
+		count int
 	}
 	type args struct {
 		item int
@@ -20,116 +60,68 @@ func TestNode_Exists(t *testing.T) {
 		args   args
 		want   bool
 	}{
-	// TODO: Add test cases.
+		{"Empty bst", fields{nil, 0}, args{5}, false},
+		{"Not exists", fields{&node{5, &node{3, nil, nil}, &node{7, &node{6, nil, nil}, nil}}, 4}, args{8}, false},
+		{"Exists", fields{&node{5, &node{3, nil, nil}, &node{7, &node{6, nil, nil}, nil}}, 4}, args{7}, true},
+		{"Exists", fields{&node{5, &node{3, nil, nil}, &node{7, &node{6, nil, nil}, nil}}, 4}, args{6}, true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			n := &Node{
-				left:  tt.fields.left,
-				right: tt.fields.right,
-				value: tt.fields.value,
+			bst := &BST{
+				root:  tt.fields.root,
+				count: tt.fields.count,
 			}
-			if got := n.Exists(tt.args.item); got != tt.want {
-				t.Errorf("Node.Exists() = %v, want %v", got, tt.want)
+			if got := bst.Exists(tt.args.item); got != tt.want {
+				t.Errorf("BST.Exists() = %v, want %v", got, tt.want)
 			}
 		})
 	}
 }
 
-func TestNode_Remove(t *testing.T) {
+func TestBST_Remove(t *testing.T) {
 	type fields struct {
-		left  *Node
-		right *Node
-		value int
+		root  *node
+		count int
 	}
 	type args struct {
 		item int
 	}
 	tests := []struct {
-		name   string
-		fields fields
-		args   args
-		want   Node
-		want1  bool
+		name      string
+		fields    fields
+		args      args
+		want      bool
+		wantCount int
+		wantState string
 	}{
-	// TODO: Add test cases.
+		{"Remove from empty tree", fields{nil, 0}, args{5}, false, 0, ""},
+		{"Remove root, replace left", fields{&node{5, &node{3, nil, nil}, nil}, 2}, args{5}, true, 1, "3"},
+		{"Remove root, replace right", fields{&node{5, nil, &node{7, nil, nil}}, 2}, args{5}, true, 1, "7"},
+		{"Remove root, replace none", fields{&node{5, nil, nil}, 1}, args{5}, true, 0, ""},
+		{"Remove non root, replace right", fields{&node{5, &node{3, nil, nil}, &node{7, &node{6, nil, nil}, nil}}, 4}, args{7}, true, 3, "3 5 6"},
+		{"Remove non root, complicated1", fields{&node{6, &node{0, &node{-1, nil, nil}, &node{3, &node{2, &node{1, nil, nil}, nil}, &node{4, nil, &node{5, nil, nil}}}}, nil}, 8}, args{0}, true, 7, "-1 1 2 3 4 5 6"},
+		{"Remove non root, complicated2", fields{&node{6, &node{0, &node{-1, nil, nil}, &node{3, &node{2, &node{1, nil, nil}, nil}, &node{4, nil, &node{5, nil, nil}}}}, nil}, 8}, args{3}, true, 7, "-1 0 1 2 4 5 6"},
+		{"Remove non root, complicated3", fields{&node{6, &node{0, &node{-1, nil, nil}, &node{3, nil, &node{4, nil, &node{5, nil, nil}}}}, nil}, 6}, args{3}, true, 5, "-1 0 4 5 6"},
+		{"Remove non root, complicated4", fields{&node{6, &node{0, nil, &node{3, &node{2, &node{1, nil, nil}, nil}, &node{4, nil, &node{5, nil, nil}}}}, nil}, 7}, args{0}, true, 6, "1 2 3 4 5 6"},
+		{"Remove non root, complicated5", fields{&node{6, &node{0, &node{-1, nil, nil}, &node{3, &node{2, &node{1, nil, nil}, nil}, &node{4, nil, &node{5, nil, nil}}}}, nil}, 8}, args{5}, true, 7, "-1 0 1 2 3 4 6"},
+		{"Remove non root, complicated6", fields{&node{6, &node{0, &node{-1, nil, nil}, &node{3, &node{2, &node{1, nil, nil}, nil}, &node{4, nil, &node{5, nil, nil}}}}, nil}, 8}, args{1}, true, 7, "-1 0 2 3 4 5 6"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			n := &Node{
-				left:  tt.fields.left,
-				right: tt.fields.right,
-				value: tt.fields.value,
+			bst := &BST{
+				root:  tt.fields.root,
+				count: tt.fields.count,
 			}
-			got, got1 := n.Remove(tt.args.item)
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Node.Remove() got = %v, want %v", got, tt.want)
+			if got := bst.Remove(tt.args.item); got != tt.want {
+				t.Errorf("BST.Remove() = %v, want %v", got, tt.want)
 			}
-			if got1 != tt.want1 {
-				t.Errorf("Node.Remove() got1 = %v, want %v", got1, tt.want1)
-			}
-		})
-	}
-}
 
-func TestNode_Add(t *testing.T) {
-	type fields struct {
-		left  *Node
-		right *Node
-		value int
-	}
-	type args struct {
-		item int
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		args   args
-		want   Node
-		want1  bool
-	}{
-	// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			n := &Node{
-				left:  tt.fields.left,
-				right: tt.fields.right,
-				value: tt.fields.value,
+			if bst.count != tt.wantCount {
+				t.Errorf("BST.Insert() = %v, want count %v", bst.count, tt.wantCount)
 			}
-			got, got1 := n.Add(tt.args.item)
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Node.Add() got = %v, want %v", got, tt.want)
-			}
-			if got1 != tt.want1 {
-				t.Errorf("Node.Add() got1 = %v, want %v", got1, tt.want1)
-			}
-		})
-	}
-}
 
-func TestNode_IsBST(t *testing.T) {
-	type fields struct {
-		left  *Node
-		right *Node
-		value int
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		want   bool
-	}{
-	// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			n := &Node{
-				left:  tt.fields.left,
-				right: tt.fields.right,
-				value: tt.fields.value,
-			}
-			if got := n.IsBST(); got != tt.want {
-				t.Errorf("Node.IsBST() = %v, want %v", got, tt.want)
+			if bst.statePrint() != tt.wantState {
+				t.Errorf("BST.Insert() = %v, want state %v", bst.statePrint(), tt.wantState)
 			}
 		})
 	}
